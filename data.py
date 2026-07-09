635fed767df8483db679bffdce75c12b986c48ad
NPol-noVA <2025007553@aurak.ac.ae>
2026-07-09 19:26:53 +0400

    Update and rename functions.py to  data.py

--- data.py
+++ data.py
@@ -5,25 +5,36 @@
 
 
 #function to put product
-def put_product (name, quantity, price) :
-    products[name] = { 
+def put_product () :
+        print("what is product's name?") 
+        name = input( 'name: ')
+        
+        print ("what is product's quantity?")
+        quantity = int(input ( 'quantity: '))
+        
+        print( "what is product's price? ") 
+        price = float(input ('price: '))
+         products[name] = { 
         'price': price ,
         'quantity': quantity 
     }
  
+
 #a function for canceling a product from list
-def cancel_product ( name): 
-    products.pop('name')
+def cancel_product(name):
+   
+    if name in products:
+        products.pop(name)
+    
+    else:
+        print("Product not found.")
 
 #a function for changing quantity of a product  
 def update_quantity (pname,newquantity):
    products[pname]['quantity'] =  newquantity 
 
-
 #function to see list of available operations
-
 def list_options ():
- 
   print( "this is list of options: ")
   
   print( "1.show products")
@@ -43,3 +54,11 @@ def search_prod ( sname):
         print (sname, " is in products' list ") 
     else:  
         print (sname, "isn't in product's list ") 
+
+
+products = {  'laptop' :{ 'price': 10000    , 'quantity': 400 },
+              'iPod'   :{ 'price': 4400     , 'quantity': 100 },
+              'iPad'   :{ 'price': 4800     , 'quantity': 500 },
+              'speaker':{ 'price': 1000     , 'quantity': 87  }, 
+           } 
+
