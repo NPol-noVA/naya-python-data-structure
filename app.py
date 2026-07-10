088c9f5ffd0ff9705ea3120f77966c34437b4f18
NPol-noVA <2025007553@aurak.ac.ae>
2026-07-10 16:50:45 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,46 +2,63 @@
 print( "Have a nice trial! welcome to this  inventory-management-system  ") 
 print( "to show list-options enter y ") 
 o = input() 
+ 
 
-
-while o == 'y': 
+while o != 'y' or o != 'Y':
+              print ("invalid! please if you want to exit enter e else enter y")
+              o= input()
+       
+while o == 'y' or 'Y': 
        
      list_options()
      print ("enter 1,2,3,4,5 for your options  ")
      x = input()
     
-    if x == '1': 
-        for name, info in products.items():
-            print(f"{name}: Price = {info['price']}, Quantity = {info['quantity']}")
+   
+     if x == '1': 
+          for name, info in products.items():
+              print(f"{name}: Price = {info['price']}, Quantity = {info['quantity']}")
     
+       
     elif x == '2':
-        print("what is product's name?") 
-        sname = input( 'name: ')
-        print ("what is product's quantity?")
-        squantity = int(input ( 'quantity: '))
-        print( "what is product's price? ") 
-        sprice = float(input ('price: '))
         put_product(sname, squantity, sprice) 
     
+
     elif x == '3':
-        print ("what product you want to update ? ") 
-        s = input()
+
+        while k == '0':
+               k == '1'
+               print ("what product you want to update ? ") 
+               s = input()
+               if search_prod(s) == False:
+                      print( "there's no", s, "in product's list shown")  
+                      k == '0'
+               
         print ("what is new quantity?") 
         new_quantity = int( input("new quantity: "))
         update_quantity(s, new_quantity)
     
     elif x == '4':
+       
         print ("what is product's name you want to search for ?") 
         s = input ()
-        search_prod (s)
+        if search_prod (s) : 
+               print( s, "is in products' list") 
+        else: 
+               print( s, "isn't in products' list")
+   
+     elif x == '5': 
+           while k == '0':
+                  k == '1'
+                  print ("what product you want to omit ? ") 
+                  s = input()
+                  if search_prod(s) == False:
+                         print( "there's no", s, "in product's list shown")  
+                         k == '0'
+           cancel_product(s)
     
-    elif x == '5': 
-        print ( "what product you want to omit? ") 
-        s=input () 
-        cancel_product(s) 
-        
-    print("enter y to continue else type n ") 
-    o = input()
-    if o == 'n': 
-      print (" ----Exit!----")   
-      break  
+     print("enter y to continue else type n ") 
+     o = tolower(input())
+     if o == 'n': 
+            print (" ----Exit!----")   
+            break  
